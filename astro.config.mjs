531f6f8a0babb4e9a11aecbc7be979cbb1cdfe69
Jose-17-11 <ariashdez@outlook.com>
2024-07-29 20:23:14 -0500

    second commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'hybrid',
+  adapter: cloudflare(),
+  integrations: [tailwind()]
+});
