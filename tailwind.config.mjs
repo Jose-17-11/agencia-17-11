33bfa99bd786ed7430173fb0e4e8f37801b2f29f
Jose-17-11 <ariashdez@outlook.com>
2024-07-31 20:42:02 -0500

    add alias and structured directories, files and add header

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,13 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'custom-bg': '#020617',
+				'custom-hover': '#6a89cc',
+				'page-bg': '#0b0e19'
+			}
+		},
 	},
 	plugins: [],
 }
