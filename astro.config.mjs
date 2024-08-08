ffc706d5d8419de5806a633d0ff30a93e92687eb
Jose-17-11 <ariashdez@outlook.com>
2024-08-08 13:53:45 -0500

    add partytown for google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import tailwind from "@astrojs/tailwind";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
   adapter: cloudflare(),
-  integrations: [tailwind()]
+  integrations: [tailwind(), partytown()]
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="astro/client" />
+interface ImportMetaEnv {
+    readonly PUBLIC_ID: string;
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
