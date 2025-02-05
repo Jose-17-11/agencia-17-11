9a1f21671b8a622081498166758c2b113a74013e
Jose-17-11 <ariashdez@outlook.com>
2025-02-04 22:21:59 -0600

    remove mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,11 @@ import partytown from "@astrojs/partytown";
 
 import react from '@astrojs/react';
 
-import mdx from '@astrojs/mdx';
-
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: cloudflare({
     imageService: 'cloudflare'
  }),
-  integrations: [tailwind(), partytown(), react(), mdx()]
+  integrations: [tailwind(), partytown(), react()]
 });
