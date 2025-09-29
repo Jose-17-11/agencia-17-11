c1d234e673d59d644bfe6113fe54eed18d929fe8
Jose-17-11 <ariashdezl@outlook.com>
2025-09-29 14:42:12 -0600

    add adapter to images and  file to settings in cloudflare "wrangler.toml"

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,5 +10,11 @@ import react from '@astrojs/react';
 export default defineConfig({
   output: 'server',
   adapter: cloudflare(),
+  image: {
+    service: {
+      entrypoint: 'astro/service/cloudflare',
+      config: {},
+    },
+  },
   integrations: [tailwind(), partytown(), react()]
 });
