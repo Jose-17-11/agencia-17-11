fa7d40d6fd36c5ee2cdb15f1c13afe4694b5f543
Jose-17-11 <ariashdezl@outlook.com>
2025-10-03 13:15:40 -0600

    add coment in file /src/common.ts

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,3 +1,5 @@
+// En este archivo definimos tipos comunes que pueden ser utilizados en múltiples componentes
+// Por ejemplo, una interfaz para los elementos de navegación
 export interface NavItem {
     url: string;
     text: string;
